b77378b4527aa0f0b79fa21c4d53df0db631a846
Justin Chang <justin.p.chang@gmail.com>
2018-09-13 13:24:14 -0400

    add draft dependency and markdown parser

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import {Editor, EditorState} from 'draft-js';
+import { EditorState } from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import createMarkdownPlugin from 'draft-js-markdown-plugin';
 
-class MyEditor extends Component {
+export default class MyEditor extends Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      plugins: [createMarkdownPlugin()]
+    };
     this.onChange = (editorState) => this.setState({editorState});
   }
 
   render() {
     return (
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+      <Editor editorState={this.state.editorState} onChange={this.onChange} plugins={this.state.plugins} />
     );
   }
 }
-
-export default MyEditor;
